88386dee832e2332e78d69f72f2b89faa02b559b
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2024-04-22 17:14:05 +0200

    suspense

--- app/update-comment/page.jsx
+++ app/update-comment/page.jsx
@@ -1,66 +1,14 @@
 "use client";
 
-import { useEffect, useState, Suspense } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-import Form from "@components/Form";
+import UpdateCommentSuspense from './suspenseUpdateComment';
 
 const UpdateComment = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const commentId = searchParams.get("id");
-
-  const [post, setPost] = useState({ comment: "", tag: "", });
-  const [submitting, setIsSubmitting] = useState(false);
-
-  useEffect(() => {
-    const getCommentDetails = async () => {
-      const response = await fetch(`/api/comment/${commentId}`);
-      const data = await response.json();
-
-      setPost({
-        comment: data.comment,
-        tag: data.tag,
-      });
-    };
-
-    if (commentId) getCommentDetails();
-  }, [commentId]);
-
-  const updateComment = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    if (!commentId) return alert("Missing CommentId!");
-
-    try {
-      const response = await fetch(`/api/comment/${commentId}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          comment: post.comment,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
+ 
   return (
     <Suspense>
-      <Form
-        type='Edit'
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updateComment}
-      />
+      <UpdateCommentSuspense />
     </Suspense>
   );
 };
---
+++ app/update-comment/suspenseUpdateComment.jsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import Form from "@components/Form";
+
+const UpdateCommentSuspense = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const commentId = searchParams.get("id");
+
+  const [post, setPost] = useState({ comment: "", tag: "", });
+  const [submitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    const getCommentDetails = async () => {
+      const response = await fetch(`/api/comment/${commentId}`);
+      const data = await response.json();
+
+      setPost({
+        comment: data.comment,
+        tag: data.tag,
+      });
+    };
+
+    if (commentId) getCommentDetails();
+  }, [commentId]);
+
+  const updateComment = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    if (!commentId) return alert("Missing CommentId!");
+
+    try {
+      const response = await fetch(`/api/comment/${commentId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          comment: post.comment,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updateComment}
+    />
+  );
+};
+
+export default UpdateCommentSuspense;
