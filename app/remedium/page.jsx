120cd4e7e67ed381871f6a131d2f21c94aabbf6b
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2025-01-07 15:40:20 +0100

    minor fix

--- app/remedium/page.jsx
+++ app/remedium/page.jsx
@@ -3,28 +3,24 @@
 import React, { useCallback, useState } from "react";
 import { Box } from '@mui/material';
 import Grid from '@mui/material/Grid2';
-import RepertorySymptomSearchView from './RepertorySymptomSearchView';
-import RepertorySymptomSelectedDataGridView from './RepertorySymptomSelectedDataGridView';
-import RepertoryRemediesDataGridView from './RepertoryRemediesDataGridView';
 
 const Repertory = () => {
 
-  const [selectedRepertorySymptomIds, setSelectedReprtorySymptomIds] = useState([]);
+  // const [selectedRepertorySymptomIds, setSelectedReprtorySymptomIds] = useState([]);
 
-  const handleSetSelectedReprtorySymptomIds = useCallback((selectedSymptomIds, remove) => {
-    if (remove) {
-      setSelectedReprtorySymptomIds(prevState => prevState.filter(item => selectedSymptomIds.every(selectedItem => selectedItem !== item)))
-    }
-    else {
-      setSelectedReprtorySymptomIds(prevState => [...prevState, ...selectedSymptomIds])
-    }
-  },[setSelectedReprtorySymptomIds])
+  // const handleSetSelectedReprtorySymptomIds = useCallback((selectedSymptomIds, remove) => {
+  //   if (remove) {
+  //     setSelectedReprtorySymptomIds(prevState => prevState.filter(item => selectedSymptomIds.every(selectedItem => selectedItem !== item)))
+  //   }
+  //   else {
+  //     setSelectedReprtorySymptomIds(prevState => [...prevState, ...selectedSymptomIds])
+  //   }
+  // },[setSelectedReprtorySymptomIds])
 
-  console.log(selectedRepertorySymptomIds, 'selectedRepertorySymptomIds')
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid size={6} >
+        {/* <Grid size={6} >
           <RepertorySymptomSearchView setSelectedReprtorySymptomIds={handleSetSelectedReprtorySymptomIds} />
         </Grid>
         <Grid size={6}>
@@ -32,14 +28,13 @@ const Repertory = () => {
             <div style={{ height: 70 }}></div>
             <RepertorySymptomSelectedDataGridView repertorySymptomIds={selectedRepertorySymptomIds} setSelectedReprtorySymptomIds={handleSetSelectedReprtorySymptomIds} />
           </Box>
-          {/* <RepertorySymptomSearchView selectedRepertorySymptomIds={selectedRepertorySymptomIds} showFilter={false} /> */}
         </Grid>
         <Grid size={6}>
           <RepertoryRemediesDataGridView repertorySymptomIds={selectedRepertorySymptomIds} />
         </Grid>
         <Grid size={6}>
           <Item>size=8</Item>
-        </Grid>
+        </Grid> */}
       </Grid>
     </Box>
   );
